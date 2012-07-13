f40284b0f6d80c4bcab145bd69b9350c4bc2a56c
Andy Elster <andy@elsternet.com>
2012-07-12 22:58:17 -0700

    more files

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ SiteLoad( "library" );
 include( 'local-my-school-manager.php' );
 $gDb = OpenDb();
 
+$test = isset( $_REQUEST['bozo'] ) ? 1 : 0;
+$gDebug = $test;
+$gTrace = $test;
+
 WriteHeader();
 DisplayMain();
 WriteFooter();
