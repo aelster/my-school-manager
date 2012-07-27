c62cb2295276a488810ce3d47b245f08e42d413c
Andy Elster <andy@elsternet.com>
2012-07-26 19:17:31 -0700

    Check authorization for left menu buttons

--- left.php
+++ left.php
@@ -6,13 +6,19 @@ END;
    }
    echo "<hr>\n";
    
+   if( $gManager ) {
+      printf( "User: %s<br>", $gUser['username'] );
+   }
+   
    if( $gActionLeft == 'menu' ) {
       echo <<<END
 <input type=button onclick="MyAddAction('Logout');" value=Logout><br>
-<input type=button onclick="MyAddAction('Users');" value=Users><br>
-<input type=button onclick="MyAddAction('Levels');" value=Levels><br>
-
+<input type=button onclick="MyAddAction('New Password');" value="New Password"><br>
 END;
+      if( UserManagerAuthorized('control') ) {
+         echo "<input type=button onclick=\"MyAddAction('Users');\" value=Users><br>";
+         echo "<input type=button onclick=\"MyAddAction('Levels');\" value=Levels><br>";
+      }
    }
    echo "<br>Left panel";
 
