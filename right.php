283eed76baa7b1b1aaa2ec88b6eea7464ebb0561
Andy Elster <andy@elsternet.com>
2012-07-27 01:45:44 -0700

    Remove comments Allow for default action

--- right.php
+++ right.php
@@ -4,46 +4,9 @@
          echo "Please select an area from the left";
          break;
       
-      case 'control':
-         UserManager('control');
-         break;
-      
-      case 'get-new-password':
-         UserManager('newpassword');
-         break;
-      
-      case 'levels':
-         UserManager('levels');
-         break;
-      
-      case 'login':
-         UserManager('login');
-         break;
-      
-      case 'resend':
-         UserManager('resend');
-         break;
-
-      case 'reset':
-         UserManager('reset');
+      default:
+         UserManager($gActionRight);
          break;
    }
-/*   
-   if( $gAction == 'Reset Password' ) {
-      UserManager('reset');
-      
-   } elseif( $gUser['pwdchanged'] == '0000-00-00 00:00:00' ) {
-		UserManager('newpassword');
-	
-   } elseif( $gAction == 'Resend' ) {
-      UserManager('resend');
-
-   } elseif( $gAction == 'Logout' ) {
-      echo "<h3>You have been sucessfully logged out</h3>";
-
-   } elseif( empty( $_SESSION['userid'] ) && $gManager ) {
-      UserManager('login');
-   }
-*/
    echo "<br>Right panel";
 ?>
